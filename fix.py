7f536bcebaeb4cb5a705a83daa53e2d1cb6dfc8a
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2019-08-21 01:53:43 -0700

    Add Samsung Internet mapping

--- fix.py
+++ fix.py
@@ -34,6 +34,22 @@ def int_or_float(string):
 	except:
 		return float(string)
 
+
+samsunginternet = {
+	True: True, False: False, None: None,
+	"1": "1.0", "2": "1.0", "3": "1.0", "4": "1.0", "5": "1.0", "6": "1.0", "7": "1.0", "8": "1.0", "9": "1.0", "10": "1.0", "11": "1.0", "12": "1.0", "13": "1.0", "14": "1.0", "15": "1.0", "16": "1.0", "17": "1.0", "18": "1.0",
+	"19": "1.5", "20": "1.5", "21": "1.5", "22": "1.5", "23": "1.5", "24": "1.5", "25": "1.5", "26": "1.5", "27": "1.5", "28": "1.5",
+	"29": "2.0", "30": "2.0", "31": "2.0", "32": "2.0", "33": "2.0", "34": "2.0",
+	"35": "3.0", "36": "3.0", "37": "3.0", "38": "3.0",
+	"39": "4.0", "40": "4.0", "41": "4.0", "42": "4.0", "43": "4.0", "44": "4.0",
+	"45": "5.0", "46": "5.0", "47": "5.0", "48": "5.0", "49": "5.0", "50": "5.0", "51": "5.0",
+	"52": "6.0", "53": "6.0", "54": "6.0", "55": "6.0", "56": "6.0",
+	"57": "7.0", "58": "7.0", "59": "7.0", "59": "7.0",
+	"60": "8.0", "61": "8.0", "62": "8.0", "63": "8.0",
+	"64": "9.0", "65": "9.0", "66": "9.0", "67": "9.0",
+	"68": False, "69": False, "70": False, "71": False, "72": False, "73": False, "74": False, "75": False, "76": False
+}
+
 def create_webview_range(value):
 	if value < 37: return "≤37"
 	return str(value)
@@ -96,10 +112,10 @@ def bump_version(value, browser):
 		
 		elif browser == 'samsunginternet_android':
 			if new_value['version_added'] != None:
-				new_value['version_added'] = bool(new_value['version_added'])
+				new_value['version_added'] = samsunginternet.get(new_value['version_added'], new_value['version_added'])
 			
 			if 'version_removed' in value and new_value['version_removed'] != None:
-				new_value['version_removed'] = bool(new_value['version_removed'])
+				new_value['version_removed'] = samsunginternet.get(new_value['version_removed'], new_value['version_removed'])
 			
 			if 'notes' in value:
 				if not isinstance(new_value['notes'], list):
