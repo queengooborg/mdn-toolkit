d33b34d7ef020c755e33ff47f77939437a0fc597
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2020-03-22 17:37:52 -0700

    Add get_nonreal.py

---
+++ get_nonreal.py
@@ -0,0 +1,31 @@
+import sys
+import subprocess
+
+if len(sys.argv) < 2:
+	print("Usage: python get_nonreal.py <folder>")
+	sys.exit(0)
+
+bcd_path = "/Users/vinyldarkscratch/Developer/git/browser-compat-data"
+browsers = ['Chrome', 'Edge', 'Firefox', 'IE', 'Opera', 'Safari']
+folder = sys.argv[1]
+print_browsers = False
+
+features = {}
+
+for browser in browsers:
+	result = subprocess.run(['node', 'scripts/traverse.js', browser.lower(), folder], cwd=bcd_path, stdout=subprocess.PIPE).stdout.decode('utf-8').split('\n')[0:-2]
+
+	for f in result:
+		feature = f.split(" (")[0]
+		if feature in features:
+			if browser not in features[feature]:
+				features[feature].append(browser)
+		else:
+			features[feature] = [browser]
+
+for feature in sorted(features.keys()):
+	print(feature)
+
+	if print_browsers:
+		for b in features[feature]:
+			print("\t- {0}".format(b))
