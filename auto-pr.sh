5917cbae1439115aafb064f0661a0f27bfb75dd9
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2021-09-04 02:40:42 -0700

    Bump collector version

--- auto-pr.sh
+++ auto-pr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-collectorversion="3.2.9"
+collectorversion="3.2.12"
 echo ""
 read -n 1 -p "PR type? ([n]ew entry/new [f]ile/real [V]alues/[c]orrections/feature [r]emoval/f[l][a]g removal (by flag/file)) " prtype
 [[ ! -z $prtype ]] && echo ""
