b06236c3ddd448df860f5f5e03f7aa96253b4a01
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2021-10-31 17:17:41 -0700

    Change browser ID for IE/Edge combination from "ie" to "ms"

--- auto-pr.sh
+++ auto-pr.sh
@@ -42,7 +42,7 @@ case $prtype in
       "Edge") browserid=edge; browser="Microsoft Edge"; break;;
       "Firefox") browserid=firefox; browser="Firefox and Firefox Android"; break;;
       "IE") browserid=ie; browser="Internet Explorer"; break;;
-      "IE/Edge") browserid=ie; browser="Internet Explorer and Edge"; break;;
+      "IE/Edge") browserid=ms; browser="Internet Explorer and Edge"; break;;
       "Opera") browserid=opera; browser="Opera and Opera Android"; break;;
       "Safari") browserid=safari; browser="Safari (Desktop and iOS/iPadOS)"; break;;
       "Safari iOS") browserid=safariios; browser="Safari iOS/iPadOS"; break;;
